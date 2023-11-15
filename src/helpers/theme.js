5f4f8a56dd618981865ddf7125031eff24b3abe0
Valeriia tymchenko <lera.tymchenko@ukr.net>
2023-11-15 22:20:36 +0200

    add app theme

--- src/helpers/theme.js
+++ src/helpers/theme.js
@@ -1,7 +1,51 @@
 const theme = {
     colors: {
-        text: "000",
+        body: "ffffff",
+        text: "121212",
+        import: "#ebebeb",
+        buttons: "#f575a1",
+        footer: "#f7d1de",
+        accentPink: "#f7d1de",
+        accentYellow: "#ffeb98",
+        accentGreen: "#bff3ee",
+        accentBlue: "#cddafa",
+        hoverButtons: "#b83d6e"
     },
-    space: [0, 2, 4, 6, 8]
+    space: [0, 2, 4, 6, 8, 16, 24, 36, 40, 56, 60, 72, 120],
+    fonts: {
+        primary: "Nunito, sans-serif",
+        secondary: "Mulish, sans-serif"
+    },
+    fontSizes: {
+        xxs: "14px",
+        xs: "16px",
+        s: "18px",
+        m: "20px",
+        l: "32px",
+        xl: "36px",
+        xxl: "48px"
+    },
+    fontWeights: {
+        regular: 400,
+        medium: 500,
+        semibild: 600,
+        bold: 700
+    },
+    lineHeights: {
+        body: 1.4,
+        mediumTitle: 1.3,
+        largeTitle: 1.2
+    },
+    borders: {
+        none: "none",
+        normal: "1px solid"
+    },
+    radii: {
+        none: 0,
+        text: 4,
+        input: 8,
+        button: 12,
+        cards: 16
+    }
 };
 export default theme;
