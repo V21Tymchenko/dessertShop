2d2693e5e2b86f31890e5a93060545ca56ef8253
Valeriia tymchenko <lera.tymchenko@ukr.net>
2023-11-20 10:52:38 +0200

    add changes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "@/components/SharedLayout";
-import HomeCakesCategory from "@/components/HomeCakesCategory/HomeCakesCategory";
+import HomePage from "./pages/HomePage/HomePage";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomeCakesCategory />} />
+          <Route index element={<HomePage />} />
           <Route path="cakes" element={<div>CAKES</div>} />
           <Route path="constructor" element={<div>CONSTRUCTOR</div>} />
           <Route path="about" element={<div>ABOUT</div>} />
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="contacts" element={<div>CONTACTS</div>} />
         </Route>
       </Routes>
-    </div>
+    </>
   );
 };
 
