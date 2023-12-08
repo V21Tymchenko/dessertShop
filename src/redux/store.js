1a64aaf562abfc3f308e144af578d8e1a7609f07
Valeriia tymchenko <lera.tymchenko@ukr.net>
2023-12-08 09:48:39 +0200

    change imports in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,7 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import { authReducer } from "./auth/auth-slice";
+import { authReducer } from "@/redux/auth/auth-slice";
 
 const authPersistConfig = {
   key: "auth",
